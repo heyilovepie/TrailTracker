dc02fa3c9763c8a5cca9d127207ae8ca8f2d90c0
Gillian Ramsay <gillianramsay@shaw.ca>
2015-03-19 15:05:27 -0700

    options popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,36 @@
 $(function(){
 	var 
 		menuToggle = $('#menu div.toggle button'),
-		overlay = $('.overlay');
+		menuOverlay = $('#menu .overlay'),
+		optionsOverlay = $('#type-overlay'),
+		newRoute = $('#new-route')
+		optionsPopup = $("#type-page");
+		optionsPopup_p = $("#type-page").parent();
 
 	menuToggle.click(function(e){
 		$(this).parent().toggleClass("open");
 		$(this).parent().parent().toggleClass("open");
 	});
 
-	overlay.click(function(e){
-		if($(e.target).is(overlay)){ //if you've clicked on the overlay part (not the menu)
+	menuOverlay.click(function(e){
+		if($(e.target).is(menuOverlay)){ //if you've clicked on the overlay part (not the menu)
 			var target = $(this).children("#menu-options");
-			for(var i = 0; i < 2; i++){ //loop twice each with  a different target
-				if(target.hasClass("open")){ 
-					target.removeClass("open");
-					target.children(".toggle").removeClass("open");
-				}
-				target = $(this).children("#menu-profile");
-			}	
+			if(target.hasClass("open")){ 
+				target.removeClass("open");
+				target.children(".toggle").removeClass("open");
+			}
 		}
-	})
+	});
+
+	optionsOverlay.click(function(e){
+		if($(e.target).is(optionsOverlay)){ //if you've clicked on the overlay part (not the menu)
+			$(this).toggleClass("hidden");
+		}
+	});
+
+	newRoute.click(function(){
+		optionsPopup_p.toggleClass("hidden");
+	});
+
+
 });
