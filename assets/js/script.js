eb018e73e6e88f1e92cee0c32ec33e29f9fd5125
Gillian Ramsay <gillianramsay@shaw.ca>
2015-03-19 10:38:15 -0700

    toggle menu changes location of button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,28 @@
 $(function(){
-	var menuToggle = $('#menu div.toggle button');
+	var 
+		menuToggle = $('#menu div.toggle button'),
+		overlay = $('.overlay');
 
 	menuToggle.click(function(e){
-		$(this).parent().toggleClass("open"); //this is not working yet
+		$(this).parent().toggleClass("open");
 		$(this).parent().parent().toggleClass("open");
 	});
+
+	overlay.click(function(e){
+		if($(e.target).is(overlay)){
+			$(this).children().toggleClass("open");
+		}
+	})
+/*
+		dialog_p.click(function(e){
+		// When the overlay is clicked, hide the dialog_p.
+		if($(e.target).is('.overlay')){
+			// This check is need to prevent
+			// bubbled up events from hiding the dialog
+			dialog_p.trigger('hide');
+		}
+	});
+*/
+
+
 });
